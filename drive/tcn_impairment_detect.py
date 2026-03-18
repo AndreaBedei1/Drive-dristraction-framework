efc72adc8451470e2adce4474b5628a21aceceea
LBacchiani <lorenzo.bacchiani2@unibo.it>
2026-03-18 15:00:29 +0100

    improve performance

--- drive/tcn_impairment_detect.py
+++ drive/tcn_impairment_detect.py
@@ -95,8 +95,8 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.cuda.manual_seed_all(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark     = False
+torch.backends.cudnn.deterministic = False #before was True
+torch.backends.cudnn.benchmark     = True #before was False
 
 # -------------------------------------------------
 # NORMALISATION & PREPROCESSING
